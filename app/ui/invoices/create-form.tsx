07af3268e7a01518c002cbd9f1987b7c35e8104d
xiaochun <487336863@qq.com>
2025-05-10 21:59:57 +0800

    feat: updaete and delete invoice

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,4 +1,4 @@
-import { CustomerField } from '@/app/lib/definitions';
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
   CheckIcon,
@@ -9,7 +9,7 @@ import {
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
 
-export default function Form({ customers }: { customers: CustomerField[] }) {
+export default function Form({ customers, invoice }: { customers: CustomerField[], invoice: InvoiceForm }) {
   return (
     <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
